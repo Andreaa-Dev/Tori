11ca83cb044fabaa00be1397e705a8a1bc2cc860
Andrea <ngocanhhsgs@gmail.com>
2022-03-13 18:16:29 +0200

    add app text input

---
+++ app/component/AppTextInput.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function AppTextInput() {
+  return (
+    <View>
+      <MaterialCommunityIcons />
+      <TextInput />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
