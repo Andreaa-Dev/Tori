8df04ab284ed21770b18881ac8739f97d6f5b00d
Andrea <ngocanhhsgs@gmail.com>
2022-03-24 23:04:27 +0200

    image input

--- app/component/ImageInput.js
+++ app/component/ImageInput.js
@@ -5,25 +5,35 @@ import * as ImagePicker from "expo-image-picker";
 
 import colour from "../config/colour";
 
-export default function ImageInput({ imageUri, handlePress }) {
-const [imageUri,setImageUri] = useState()
-  const handlePress = () =>{
-    if (!imageUri)
-  }
-  const selectImage = async () =>{
-    try{const result = await ImagePicker.launchImageLibraryAsync()
-    if (!result.cancelled) setImageUri}
-  }
-  return (
-    <TouchableWithoutFeedback onPress={handlePress}>
-      <View style={styles.container}>
-        {!imageUri && (
-          <MaterialCommunityIcons color={colour.medium} name="camera" />
-        )}
-        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-      </View>
-    </TouchableWithoutFeedback>
-  );
+export default function ImageInput({ imageUri, onChangeImage }) {
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (error) {
+      console.log("error");
+    }
+
+    return (
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <View style={styles.container}>
+          {!imageUri && (
+            <MaterialCommunityIcons color={colour.medium} name="camera" />
+          )}
+          {imageUri && (
+            <Image source={{ uri: imageUri }} style={styles.image} />
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
