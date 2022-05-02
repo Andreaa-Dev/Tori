7aa9c22a94deb328a5091f258f61ff6853ff5818
Andrea <ngocanhhsgs@gmail.com>
2022-05-02 15:48:27 +0300

    add loading component

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -7,18 +7,24 @@ import colour from "../config/colour";
 import routes from "../navigation/routes";
 import listingsApi from "../api/listing";
 import AppText from "../component/AppText";
+import Button from "../component/AppButton";
+import ActivityIndicator from "../component/ActivityIndicator";
 
 export default function ListingScreen({ navigation }) {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await listingsApi.getListings();
+    setLoading(false);
     if (!response.ok) return setError(true);
+
     setError(false);
     setListings(response.data);
   };
@@ -31,6 +37,7 @@ export default function ListingScreen({ navigation }) {
           <Button title="Retry" onPress={loadListings} />
         </>
       )}
+      <ActivityIndicator visible={true} />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
